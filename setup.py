f4595329c9aa9565f40a9c450e51b5030dbf881a
princekrroshan01 <princekrroshan01@gmail.com>
2020-04-02 18:11:31 +0530

    some change to avoid build fail

--- setup.py
+++ setup.py
@@ -6,16 +6,16 @@ def read(fname):
 
 setup(
     name="flask-starter",
-    version="0.1",
+    version="0.2",
     author="Prince Roshan",
     author_email="princekrroshan01@gmail.com",
     url="https://github.com/princekrroshan01/flask-starter",
-    description=(""),
+    description=("A flask extension which contains a basic app and is configured in your local machine through a command line utility "),
     long_description=read('README.md'),
     license="MIT",
     py_modules=['script'],
     entry_points={'console_scripts': ['flask-starter-project = script:main']},
-    install_requires=['flask','requests','httplib2','click','shutil'],
+    install_requires=['flask','requests','httplib2','click'],
     include_package_data=True
 )
 
