35b019b89c25903c76c1bf82be1d9de84efc6644
Prince Roshan <princekrroshan01@gmail.com>
2023-05-24 19:13:06 +0530

    Fix setup script

--- script.py
+++ script.py
@@ -3,12 +3,22 @@ import shutil
 
 import click
 
+def move(source_directory,destination_directory):
+    items = os.listdir(source_directory)
+    
+    # Move each item to the destination directory
+    for item in items:
+        source_path = os.path.join(source_directory, item)
+        destination_path = os.path.join(destination_directory, item)
+        shutil.move(source_path, destination_path)
 
 @click.command()
 @click.option("--name", help="Project name")
 def main(name):
     os.system("git init")
     os.system("git clone https://github.com/princekrroshan01/flask-starter.git")
+    os.mkdir(name)
+    move("flask-starter",name)
     os.remove(f"{name}/script.py")
     os.remove(f"{name}/setup.py")
     os.remove(f"{name}/LICENSE")
@@ -16,6 +26,7 @@ def main(name):
     os.remove(f"{name}/README.rst")
     os.remove(f"{name}/requirements.txt")
     shutil.rmtree(f"{name}/.github")
+    shutil.rmtree("flask-starter")
 
 
 if __name__ == "__main__":
